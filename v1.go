725bf9f06cb5da8ac7826751a7e772ea6c0f3de1
Christopher Tarry <chrisjtarry@gmail.com>
2025-06-16 13:36:31 -0400

    reenable fields

--- v1.go
+++ v1.go
@@ -20,165 +20,165 @@ func prepareContract(addr types.Address, endHeight uint64) types.FileContract {
 }
 
 func (f *fuzzer) generateTransaction() (txn types.Transaction) {
-	// {
-	// 	count := f.rng.Intn(2)
-	// 	for _, fce := range mapValues(f.fces) {
-	// 		if len(txn.StorageProofs) >= count {
-	// 			break
-	// 		}
+	{
+		count := f.rng.Intn(2)
+		for _, fce := range mapValues(f.fces) {
+			if len(txn.StorageProofs) >= count {
+				break
+			}
 
-	// 		id := fce.ID
-	// 		fc := fce.FileContract
-	// 		height := f.n.tip().Height
-	// 		if height < fc.WindowStart {
-	// 			continue
-	// 		}
-	// 		txn.StorageProofs = append(txn.StorageProofs, types.StorageProof{
-	// 			ParentID: id,
-	// 		})
-	// 		delete(f.fces, id)
-	// 	}
-	// 	if len(txn.StorageProofs) > 0 {
-	// 		// can't have storage proofs and outputs in one transaction
-	// 		return
-	// 	}
-	// }
-	// var amount types.Currency
-	// {
-	// 	for i, count := 0, f.rng.Intn(3); i < count; i++ {
-	// 		fc := prepareContract(f.addr, f.n.tip().Height+10)
-	// 		txn.FileContracts = append(txn.FileContracts, fc)
-	// 		amount = amount.Add(fc.Payout)
-	// 	}
-	// }
-	// {
-	// 	i := 0
-	// 	count := f.rng.Intn(3)
-	// 	for _, fce := range mapValues(f.fces) {
-	// 		if i > count {
-	// 			break
-	// 		}
+			id := fce.ID
+			fc := fce.FileContract
+			height := f.n.tip().Height
+			if height < fc.WindowStart {
+				continue
+			}
+			txn.StorageProofs = append(txn.StorageProofs, types.StorageProof{
+				ParentID: id,
+			})
+			delete(f.fces, id)
+		}
+		if len(txn.StorageProofs) > 0 {
+			// can't have storage proofs and outputs in one transaction
+			return
+		}
+	}
+	var amount types.Currency
+	{
+		for i, count := 0, f.rng.Intn(3); i < count; i++ {
+			fc := prepareContract(f.addr, f.n.tip().Height+10)
+			txn.FileContracts = append(txn.FileContracts, fc)
+			amount = amount.Add(fc.Payout)
+		}
+	}
+	{
+		i := 0
+		count := f.rng.Intn(3)
+		for _, fce := range mapValues(f.fces) {
+			if i > count {
+				break
+			}
 
-	// 		fc := fce.FileContract
-	// 		height := f.n.tip().Height
-	// 		if fc.WindowStart >= height {
-	// 			continue
-	// 		}
-	// 		fc.RevisionNumber++
-	// 		// fc.WindowStart = height + 1
-	// 		// fc.WindowEnd = fc.WindowStart + 10
-	// 		txn.FileContractRevisions = append(txn.FileContractRevisions, types.FileContractRevision{
-	// 			ParentID:         fce.ID,
-	// 			UnlockConditions: f.uc,
-	// 			FileContract:     fc,
-	// 		})
-	// 		f.fces[fce.ID] = types.FileContractElement{
-	// 			ID:           fce.ID,
-	// 			StateElement: fce.StateElement,
-	// 			FileContract: fc,
-	// 		}
+			fc := fce.FileContract
+			height := f.n.tip().Height
+			if fc.WindowStart >= height {
+				continue
+			}
+			fc.RevisionNumber++
+			// fc.WindowStart = height + 1
+			// fc.WindowEnd = fc.WindowStart + 10
+			txn.FileContractRevisions = append(txn.FileContractRevisions, types.FileContractRevision{
+				ParentID:         fce.ID,
+				UnlockConditions: f.uc,
+				FileContract:     fc,
+			})
+			f.fces[fce.ID] = types.FileContractElement{
+				ID:           fce.ID,
+				StateElement: fce.StateElement,
+				FileContract: fc,
+			}
 
-	// 		i++
-	// 	}
-	// }
-	// {
-	// 	for i, count := 0, f.rng.Intn(3); i < count; i++ {
-	// 		sco := types.SiacoinOutput{
-	// 			Address: f.addr,
-	// 			Value:   types.NewCurrency64(1),
-	// 		}
+			i++
+		}
+	}
+	{
+		for i, count := 0, f.rng.Intn(3); i < count; i++ {
+			sco := types.SiacoinOutput{
+				Address: f.addr,
+				Value:   types.NewCurrency64(1),
+			}
 
-	// 		amount = amount.Add(sco.Value)
-	// 		txn.SiacoinOutputs = append(txn.SiacoinOutputs, sco)
-	// 	}
-	// 	if amount.Cmp(types.ZeroCurrency) == 1 {
-	// 		var sum types.Currency
-	// 		for _, sce := range mapValues(f.sces) {
-	// 			id := sce.ID
-	// 			sum = sum.Add(sce.SiacoinOutput.Value)
-	// 			txn.SiacoinInputs = append(txn.SiacoinInputs, types.SiacoinInput{
-	// 				ParentID:         id,
-	// 				UnlockConditions: f.uc,
-	// 			})
-	// 			delete(f.sces, id)
+			amount = amount.Add(sco.Value)
+			txn.SiacoinOutputs = append(txn.SiacoinOutputs, sco)
+		}
+		if amount.Cmp(types.ZeroCurrency) == 1 {
+			var sum types.Currency
+			for _, sce := range mapValues(f.sces) {
+				id := sce.ID
+				sum = sum.Add(sce.SiacoinOutput.Value)
+				txn.SiacoinInputs = append(txn.SiacoinInputs, types.SiacoinInput{
+					ParentID:         id,
+					UnlockConditions: f.uc,
+				})
+				delete(f.sces, id)
 
-	// 			if sum.Cmp(amount) >= 0 {
-	// 				break
-	// 			}
-	// 		}
-	// 		if sum.Cmp(amount) == 1 {
-	// 			txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
-	// 				Address: f.addr,
-	// 				Value:   sum.Sub(amount),
-	// 			})
-	// 		}
-	// 	}
-	// }
-	// {
-	// 	var amount uint64
-	// 	for i, count := 0, f.rng.Intn(3); i < count; i++ {
-	// 		sfo := types.SiafundOutput{
-	// 			Address: f.addr,
-	// 			Value:   1,
-	// 		}
+				if sum.Cmp(amount) >= 0 {
+					break
+				}
+			}
+			if sum.Cmp(amount) == 1 {
+				txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
+					Address: f.addr,
+					Value:   sum.Sub(amount),
+				})
+			}
+		}
+	}
+	{
+		var amount uint64
+		for i, count := 0, f.rng.Intn(3); i < count; i++ {
+			sfo := types.SiafundOutput{
+				Address: f.addr,
+				Value:   1,
+			}
 
-	// 		amount += sfo.Value
-	// 		txn.SiafundOutputs = append(txn.SiafundOutputs, sfo)
-	// 	}
-	// 	if amount > 0 {
-	// 		var sum uint64
-	// 		for _, sfe := range mapValues(f.sfes) {
-	// 			id := sfe.ID
-	// 			sum += sfe.SiafundOutput.Value
-	// 			txn.SiafundInputs = append(txn.SiafundInputs, types.SiafundInput{
-	// 				ParentID:         id,
-	// 				UnlockConditions: f.uc,
-	// 			})
-	// 			delete(f.sfes, id)
+			amount += sfo.Value
+			txn.SiafundOutputs = append(txn.SiafundOutputs, sfo)
+		}
+		if amount > 0 {
+			var sum uint64
+			for _, sfe := range mapValues(f.sfes) {
+				id := sfe.ID
+				sum += sfe.SiafundOutput.Value
+				txn.SiafundInputs = append(txn.SiafundInputs, types.SiafundInput{
+					ParentID:         id,
+					UnlockConditions: f.uc,
+				})
+				delete(f.sfes, id)
 
-	// 			if sum >= amount {
-	// 				break
-	// 			}
-	// 		}
-	// 		if sum > amount {
-	// 			txn.SiafundOutputs = append(txn.SiafundOutputs, types.SiafundOutput{
-	// 				Address: f.addr,
-	// 				Value:   sum - amount,
-	// 			})
-	// 		}
-	// 	}
-	// }
-	// signTransactionWithContracts(f.n.tipState(), f.pk, &txn)
+				if sum >= amount {
+					break
+				}
+			}
+			if sum > amount {
+				txn.SiafundOutputs = append(txn.SiafundOutputs, types.SiafundOutput{
+					Address: f.addr,
+					Value:   sum - amount,
+				})
+			}
+		}
+	}
+	signTransactionWithContracts(f.n.tipState(), f.pk, &txn)
 
-	// for i, sco := range txn.SiacoinOutputs {
-	// 	id := txn.SiacoinOutputID(i)
-	// 	f.sces[id] = types.SiacoinElement{
-	// 		ID: id,
-	// 		StateElement: types.StateElement{
-	// 			LeafIndex: types.UnassignedLeafIndex,
-	// 		},
-	// 		SiacoinOutput: sco,
-	// 	}
-	// }
-	// for i, sfo := range txn.SiafundOutputs {
-	// 	id := txn.SiafundOutputID(i)
-	// 	f.sfes[id] = types.SiafundElement{
-	// 		ID: id,
-	// 		StateElement: types.StateElement{
-	// 			LeafIndex: types.UnassignedLeafIndex,
-	// 		},
-	// 		SiafundOutput: sfo,
-	// 	}
-	// }
-	// for i, fc := range txn.FileContracts {
-	// 	id := txn.FileContractID(i)
-	// 	f.fces[id] = types.FileContractElement{
-	// 		ID: id,
-	// 		StateElement: types.StateElement{
-	// 			LeafIndex: types.UnassignedLeafIndex,
-	// 		},
-	// 		FileContract: fc,
-	// 	}
-	// }
+	for i, sco := range txn.SiacoinOutputs {
+		id := txn.SiacoinOutputID(i)
+		f.sces[id] = types.SiacoinElement{
+			ID: id,
+			StateElement: types.StateElement{
+				LeafIndex: types.UnassignedLeafIndex,
+			},
+			SiacoinOutput: sco,
+		}
+	}
+	for i, sfo := range txn.SiafundOutputs {
+		id := txn.SiafundOutputID(i)
+		f.sfes[id] = types.SiafundElement{
+			ID: id,
+			StateElement: types.StateElement{
+				LeafIndex: types.UnassignedLeafIndex,
+			},
+			SiafundOutput: sfo,
+		}
+	}
+	for i, fc := range txn.FileContracts {
+		id := txn.FileContractID(i)
+		f.fces[id] = types.FileContractElement{
+			ID: id,
+			StateElement: types.StateElement{
+				LeafIndex: types.UnassignedLeafIndex,
+			},
+			FileContract: fc,
+		}
+	}
 	return
 }
--- v2.go
+++ v2.go
@@ -59,43 +59,43 @@ func (f *fuzzer) generateV2Transaction(originalParents map[types.FileContractID]
 			txn.FileContracts = append(txn.FileContracts, fc)
 		}
 	}
-	// {
-	// 	i := 0
-	// 	count := f.rng.Intn(3)
-	// 	for _, fce := range mapValues(f.v2fces) {
-	// 		if i > count {
-	// 			break
-	// 		}
+	{
+		i := 0
+		count := f.rng.Intn(3)
+		for _, fce := range mapValues(f.v2fces) {
+			if i > count {
+				break
+			}
 
-	// 		id := fce.ID
-	// 		fc := fce.V2FileContract
-	// 		height := f.n.tip().Height
-	// 		if height >= fc.ProofHeight {
-	// 			continue
-	// 		}
-	// 		fc.RevisionNumber++
+			id := fce.ID
+			fc := fce.V2FileContract
+			height := f.n.tip().Height
+			if height >= fc.ProofHeight {
+				continue
+			}
+			fc.RevisionNumber++
 
-	// 		parent := fce
-	// 		if v, ok := originalParents[id]; ok {
-	// 			parent = v
-	// 		} else {
-	// 			originalParents[id] = parent
-	// 		}
+			parent := fce
+			if v, ok := originalParents[id]; ok {
+				parent = v
+			} else {
+				originalParents[id] = parent
+			}
 
-	// 		txn.FileContractRevisions = append(txn.FileContractRevisions, types.V2FileContractRevision{
-	// 			Parent:   parent,
-	// 			Revision: fc,
-	// 		})
+			txn.FileContractRevisions = append(txn.FileContractRevisions, types.V2FileContractRevision{
+				Parent:   parent,
+				Revision: fc,
+			})
 
-	// 		f.v2fces[id] = types.V2FileContractElement{
-	// 			ID:             id,
-	// 			StateElement:   fce.StateElement,
-	// 			V2FileContract: fc,
-	// 		}
+			f.v2fces[id] = types.V2FileContractElement{
+				ID:             id,
+				StateElement:   fce.StateElement,
+				V2FileContract: fc,
+			}
 
-	// 		i++
-	// 	}
-	// }
+			i++
+		}
+	}
 	{
 		i := 0
 		count := f.rng.Intn(3)
@@ -130,91 +130,91 @@ func (f *fuzzer) generateV2Transaction(originalParents map[types.FileContractID]
 			i++
 		}
 	}
-	// {
-	// 	i := 0
-	// 	count := f.rng.Intn(3)
-	// 	for _, fce := range mapValues(f.v2fces) {
-	// 		if i > count {
-	// 			break
-	// 		}
-
-	// 		id := fce.ID
-	// 		fc := fce.V2FileContract
-	// 		height := f.n.tip().Height
-	// 		if height < fc.ProofHeight {
-	// 			continue
-	// 		}
-	// 		fc.RevisionNumber++
-
-	// 		parent := fce
-	// 		if v, ok := originalParents[id]; ok {
-	// 			parent = v
-	// 		} else {
-	// 			originalParents[id] = parent
-	// 		}
-
-	// 		newContract := fc
-	// 		newContract.ProofHeight = height + 10
-	// 		newContract.ExpirationHeight = newContract.ProofHeight + 1
-	// 		renewal := &types.V2FileContractRenewal{
-	// 			FinalRenterOutput: fc.RenterOutput,
-	// 			FinalHostOutput:   fc.HostOutput,
-	// 			RenterRollover:    types.ZeroCurrency,
-	// 			HostRollover:      types.ZeroCurrency,
-	// 			NewContract:       newContract,
-	// 		}
-	// 		txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
-	// 			Parent:     parent,
-	// 			Resolution: renewal,
-	// 		})
-	// 		amount = amount.Add(payoutV2(fc))
-	// 		delete(f.v2fces, id)
-
-	// 		i++
-	// 	}
-	// }
-	// {
-	// 	i := 0
-	// 	count := f.rng.Intn(3)
-	// 	for _, fce := range mapValues(f.v2fces) {
-	// 		if i > count {
-	// 			break
-	// 		}
-
-	// 		id := fce.ID
-	// 		fc := fce.V2FileContract
-	// 		height := f.n.tip().Height
-	// 		if height < fc.ExpirationHeight {
-	// 			continue
-	// 		}
-	// 		fc.RevisionNumber++
-
-	// 		parent := fce.Copy()
-	// 		if v, ok := originalParents[id]; ok {
-	// 			parent = v
-	// 		} else {
-	// 			originalParents[id] = parent
-	// 		}
-
-	// 		txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
-	// 			Parent:     parent,
-	// 			Resolution: &types.V2FileContractExpiration{},
-	// 		})
-	// 		delete(f.v2fces, id)
-
-	// 		i++
-	// 	}
-	// }
 	{
-		// for i, count := 0, f.rng.Intn(3); i < count; i++ {
-		// 	sco := types.SiacoinOutput{
-		// 		Address: f.addr,
-		// 		Value:   types.NewCurrency64(1),
-		// 	}
+		i := 0
+		count := f.rng.Intn(3)
+		for _, fce := range mapValues(f.v2fces) {
+			if i > count {
+				break
+			}
 
-		// 	amount = amount.Add(sco.Value)
-		// 	txn.SiacoinOutputs = append(txn.SiacoinOutputs, sco)
-		// }
+			id := fce.ID
+			fc := fce.V2FileContract
+			height := f.n.tip().Height
+			if height < fc.ProofHeight {
+				continue
+			}
+			fc.RevisionNumber++
+
+			parent := fce
+			if v, ok := originalParents[id]; ok {
+				parent = v
+			} else {
+				originalParents[id] = parent
+			}
+
+			newContract := fc
+			newContract.ProofHeight = height + 10
+			newContract.ExpirationHeight = newContract.ProofHeight + 1
+			renewal := &types.V2FileContractRenewal{
+				FinalRenterOutput: fc.RenterOutput,
+				FinalHostOutput:   fc.HostOutput,
+				RenterRollover:    types.ZeroCurrency,
+				HostRollover:      types.ZeroCurrency,
+				NewContract:       newContract,
+			}
+			txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
+				Parent:     parent,
+				Resolution: renewal,
+			})
+			amount = amount.Add(payoutV2(fc))
+			delete(f.v2fces, id)
+
+			i++
+		}
+	}
+	{
+		i := 0
+		count := f.rng.Intn(3)
+		for _, fce := range mapValues(f.v2fces) {
+			if i > count {
+				break
+			}
+
+			id := fce.ID
+			fc := fce.V2FileContract
+			height := f.n.tip().Height
+			if height < fc.ExpirationHeight {
+				continue
+			}
+			fc.RevisionNumber++
+
+			parent := fce.Copy()
+			if v, ok := originalParents[id]; ok {
+				parent = v
+			} else {
+				originalParents[id] = parent
+			}
+
+			txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
+				Parent:     parent,
+				Resolution: &types.V2FileContractExpiration{},
+			})
+			delete(f.v2fces, id)
+
+			i++
+		}
+	}
+	{
+		for i, count := 0, f.rng.Intn(3); i < count; i++ {
+			sco := types.SiacoinOutput{
+				Address: f.addr,
+				Value:   types.NewCurrency64(1),
+			}
+
+			amount = amount.Add(sco.Value)
+			txn.SiacoinOutputs = append(txn.SiacoinOutputs, sco)
+		}
 		if amount.Cmp(types.ZeroCurrency) == 1 {
 			var sum types.Currency
 			for _, sce := range mapValues(f.sces) {
@@ -237,39 +237,39 @@ func (f *fuzzer) generateV2Transaction(originalParents map[types.FileContractID]
 			}
 		}
 	}
-	// {
-	// 	var amount uint64
-	// 	for i, count := 0, f.rng.Intn(3); i < count; i++ {
-	// 		sfo := types.SiafundOutput{
-	// 			Address: f.addr,
-	// 			Value:   1,
-	// 		}
+	{
+		var amount uint64
+		for i, count := 0, f.rng.Intn(3); i < count; i++ {
+			sfo := types.SiafundOutput{
+				Address: f.addr,
+				Value:   1,
+			}
 
-	// 		amount += sfo.Value
-	// 		txn.SiafundOutputs = append(txn.SiafundOutputs, sfo)
-	// 	}
-	// 	if amount > 0 {
-	// 		var sum uint64
-	// 		for _, sfe := range mapValues(f.sfes) {
-	// 			sum += sfe.SiafundOutput.Value
-	// 			txn.SiafundInputs = append(txn.SiafundInputs, types.V2SiafundInput{
-	// 				Parent:          sfe,
-	// 				SatisfiedPolicy: types.SatisfiedPolicy{Policy: f.policy},
-	// 			})
-	// 			delete(f.sfes, sfe.ID)
+			amount += sfo.Value
+			txn.SiafundOutputs = append(txn.SiafundOutputs, sfo)
+		}
+		if amount > 0 {
+			var sum uint64
+			for _, sfe := range mapValues(f.sfes) {
+				sum += sfe.SiafundOutput.Value
+				txn.SiafundInputs = append(txn.SiafundInputs, types.V2SiafundInput{
+					Parent:          sfe,
+					SatisfiedPolicy: types.SatisfiedPolicy{Policy: f.policy},
+				})
+				delete(f.sfes, sfe.ID)
 
-	// 			if sum >= amount {
-	// 				break
-	// 			}
-	// 		}
-	// 		if sum > amount {
-	// 			txn.SiafundOutputs = append(txn.SiafundOutputs, types.SiafundOutput{
-	// 				Address: f.addr,
-	// 				Value:   sum - amount,
-	// 			})
-	// 		}
-	// 	}
-	// }
+				if sum >= amount {
+					break
+				}
+			}
+			if sum > amount {
+				txn.SiafundOutputs = append(txn.SiafundOutputs, types.SiafundOutput{
+					Address: f.addr,
+					Value:   sum - amount,
+				})
+			}
+		}
+	}
 	// so we don't get "transactions cannot be empty"
 	txn.ArbitraryData = []byte("1234")
 	signV2TransactionWithContracts(f.n.tipState(), f.pk, &txn)
