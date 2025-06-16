5987b1684e9d08727113e2a95c3d4e42e0c1e086
Christopher Tarry <chrisjtarry@gmail.com>
2025-06-15 23:25:16 -0400

    split into different files

---
+++ fuzz.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"go.sia.tech/core/consensus"
+	"go.sia.tech/core/types"
+)
+
+type fuzzer struct {
+	rng *rand.Rand
+	n   *testChain
+
+	pk     types.PrivateKey
+	uc     types.UnlockConditions
+	addr   types.Address
+	policy types.SpendPolicy
+
+	cies   []types.ChainIndexElement
+	sces   map[types.SiacoinOutputID]types.SiacoinElement
+	sfes   map[types.SiafundOutputID]types.SiafundElement
+	fces   map[types.FileContractID]types.FileContractElement
+	v2fces map[types.FileContractID]types.V2FileContractElement
+}
+
+func newFuzzer(rng *rand.Rand, pk types.PrivateKey) *fuzzer {
+	uc := types.StandardUnlockConditions(pk.PublicKey())
+	addr := uc.UnlockHash()
+
+	n := newTestChain(false, func(network *consensus.Network, genesisBlock types.Block) {
+		network.HardforkV2.AllowHeight = 100
+		network.HardforkV2.RequireHeight = 500
+		genesisBlock.Transactions[0].SiacoinOutputs[0].Address = addr
+		genesisBlock.Transactions[0].SiafundOutputs[0].Address = addr
+	})
+
+	f := &fuzzer{
+		n: n,
+
+		rng: rng,
+
+		pk:     pk,
+		uc:     uc,
+		addr:   addr,
+		policy: types.SpendPolicy(types.SpendPolicy{Type: types.PolicyTypeUnlockConditions(uc)}),
+
+		sces:   make(map[types.SiacoinOutputID]types.SiacoinElement),
+		sfes:   make(map[types.SiafundOutputID]types.SiafundElement),
+		fces:   make(map[types.FileContractID]types.FileContractElement),
+		v2fces: make(map[types.FileContractID]types.V2FileContractElement),
+	}
+
+	bs := consensus.V1BlockSupplement{Transactions: make([]consensus.V1TransactionSupplement, len(n.genesis().Transactions))}
+	_, au := consensus.ApplyBlock(n.network.GenesisState(), n.genesis(), bs, time.Time{})
+	f.processApplyUpdate(au)
+
+	return f
+}
+
+func (f *fuzzer) prob(p float64) bool {
+	return f.rng.Float64() < p
+}
+
+func (f *fuzzer) applyBlock(b types.Block) {
+	au := f.n.applyBlock(b)
+	f.processApplyUpdate(au)
+}
+
+func (f *fuzzer) revertBlock() {
+	ru := f.n.revertBlock()
+	f.processRevertUpdate(ru)
+}
+
+func (f *fuzzer) mineBlock() {
+	var txns []types.Transaction
+	if f.n.tip().Height < (f.n.network.HardforkV2.RequireHeight - 1) {
+		for i := 0; i < f.rng.Intn(20); i++ {
+			txns = append(txns, f.generateTransaction())
+		}
+	}
+
+	var v2Txns []types.V2Transaction
+	if f.n.tip().Height >= f.n.network.HardforkV2.AllowHeight {
+		originalParents := make(map[types.FileContractID]types.V2FileContractElement)
+		for i := 0; i < f.rng.Intn(20); i++ {
+			v2Txns = append(v2Txns, f.generateV2Transaction(originalParents))
+		}
+	}
+
+	b := mineBlock(f.n.tipState(), txns, v2Txns, types.VoidAddress)
+	f.applyBlock(b)
+}
+
+func (f *fuzzer) processApplyUpdate(au consensus.ApplyUpdate) {
+	for _, diff := range au.SiacoinElementDiffs() {
+		if diff.SiacoinElement.SiacoinOutput.Address != f.addr {
+			continue
+		} else if diff.Created && diff.Spent {
+			continue
+		}
+
+		id := diff.SiacoinElement.ID
+		if diff.Spent {
+			delete(f.sces, id)
+		} else {
+			f.sces[id] = diff.SiacoinElement.Copy()
+		}
+	}
+	for _, diff := range au.SiafundElementDiffs() {
+		if diff.SiafundElement.SiafundOutput.Address != f.addr {
+			continue
+		} else if diff.Created && diff.Spent {
+			continue
+		}
+
+		id := diff.SiafundElement.ID
+		if diff.Spent {
+			delete(f.sfes, id)
+		} else {
+			f.sfes[id] = diff.SiafundElement.Copy()
+		}
+	}
+	for _, diff := range au.FileContractElementDiffs() {
+		id := diff.FileContractElement.ID
+		if diff.Created {
+			f.fces[id] = diff.FileContractElement.Copy()
+		} else if diff.Revision != nil {
+			diff.FileContractElement.FileContract = *diff.Revision
+			f.fces[id] = diff.FileContractElement.Copy()
+		} else if diff.Resolved {
+			delete(f.fces, id)
+		}
+	}
+	for _, diff := range au.V2FileContractElementDiffs() {
+		id := diff.V2FileContractElement.ID
+		if diff.Created {
+			f.v2fces[id] = diff.V2FileContractElement.Copy()
+		} else if diff.Revision != nil {
+			diff.V2FileContractElement.V2FileContract = *diff.Revision
+			f.v2fces[id] = diff.V2FileContractElement.Copy()
+		} else if diff.Resolution != nil {
+			delete(f.v2fces, id)
+		}
+	}
+	f.cies = append(f.cies, au.ChainIndexElement())
+
+	for id, sce := range f.sces {
+		au.UpdateElementProof(&sce.StateElement)
+		f.sces[id] = sce.Copy()
+	}
+	for id, sfe := range f.sfes {
+		au.UpdateElementProof(&sfe.StateElement)
+		f.sfes[id] = sfe.Copy()
+	}
+	for id, fce := range f.fces {
+		au.UpdateElementProof(&fce.StateElement)
+		f.fces[id] = fce.Copy()
+	}
+	for id, fce := range f.v2fces {
+		au.UpdateElementProof(&fce.StateElement)
+		f.v2fces[id] = fce.Copy()
+	}
+	for i, cie := range f.cies {
+		au.UpdateElementProof(&cie.StateElement)
+		f.cies[i] = cie
+	}
+}
+
+func (f *fuzzer) processRevertUpdate(ru consensus.RevertUpdate) {
+	for _, diff := range ru.SiacoinElementDiffs() {
+		if diff.SiacoinElement.SiacoinOutput.Address != f.addr {
+			continue
+		} else if diff.Created && diff.Spent {
+			continue
+		}
+
+		id := diff.SiacoinElement.ID
+		if diff.Spent {
+			f.sces[id] = diff.SiacoinElement.Copy()
+		} else {
+			delete(f.sces, id)
+		}
+	}
+	for _, diff := range ru.SiafundElementDiffs() {
+		if diff.SiafundElement.SiafundOutput.Address != f.addr {
+			continue
+		} else if diff.Created && diff.Spent {
+			continue
+		}
+
+		id := diff.SiafundElement.ID
+		if diff.Spent {
+			f.sfes[id] = diff.SiafundElement.Copy()
+		} else {
+			delete(f.sfes, id)
+		}
+	}
+	for _, diff := range ru.FileContractElementDiffs() {
+		id := diff.FileContractElement.ID
+		if diff.Created {
+			delete(f.fces, id)
+		} else if diff.Revision != nil {
+			f.fces[id] = diff.FileContractElement.Copy()
+		} else if diff.Resolved {
+			f.fces[id] = diff.FileContractElement.Copy()
+		}
+	}
+	for _, diff := range ru.V2FileContractElementDiffs() {
+		id := diff.V2FileContractElement.ID
+		if diff.Created {
+			delete(f.v2fces, id)
+		} else if diff.Revision != nil {
+			f.v2fces[id] = diff.V2FileContractElement.Copy()
+		} else if diff.Resolution != nil {
+			f.v2fces[id] = diff.V2FileContractElement.Copy()
+		}
+	}
+	f.cies = f.cies[:len(f.cies)-1]
+
+	for id, sce := range f.sces {
+		ru.UpdateElementProof(&sce.StateElement)
+		f.sces[id] = sce.Copy()
+	}
+	for id, sfe := range f.sfes {
+		ru.UpdateElementProof(&sfe.StateElement)
+		f.sfes[id] = sfe.Copy()
+	}
+	for id, fce := range f.fces {
+		ru.UpdateElementProof(&fce.StateElement)
+		f.fces[id] = fce.Copy()
+	}
+	for id, fce := range f.v2fces {
+		ru.UpdateElementProof(&fce.StateElement)
+		f.v2fces[id] = fce.Copy()
+	}
+	for i, cie := range f.cies {
+		ru.UpdateElementProof(&cie.StateElement)
+		f.cies[i] = cie
+	}
+}
---
+++ v2.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"go.sia.tech/core/consensus"
+	proto4 "go.sia.tech/core/rhp/v4"
+	"go.sia.tech/core/types"
+)
+
+func payoutV2(fc types.V2FileContract) types.Currency {
+	return fc.RenterOutput.Value.Add(fc.HostOutput.Value).Add(consensus.State{}.V2FileContractTax(fc))
+}
+
+func prepareV2Contract(renterPK, hostPK types.PrivateKey, proofHeight uint64) (types.V2FileContract, types.Currency) {
+	fc, _ := proto4.NewContract(proto4.HostPrices{}, proto4.RPCFormContractParams{
+		ProofHeight:     proofHeight,
+		Allowance:       types.Siacoins(1),
+		RenterAddress:   types.StandardUnlockConditions(renterPK.PublicKey()).UnlockHash(),
+		Collateral:      types.Siacoins(1),
+		RenterPublicKey: renterPK.PublicKey(),
+	}, hostPK.PublicKey(), types.StandardUnlockConditions(hostPK.PublicKey()).UnlockHash())
+	fc.ExpirationHeight = fc.ProofHeight + 1
+	fc.RenterOutput.Address = types.VoidAddress
+	fc.HostOutput.Address = types.VoidAddress
+
+	return fc, payoutV2(fc)
+}
+
+func (f *fuzzer) generateV2Transaction(originalParents map[types.FileContractID]types.V2FileContractElement) (txn types.V2Transaction) {
+	var amount types.Currency
+	{
+		for i, count := 0, f.rng.Intn(3); i < count; i++ {
+			fc, payout := prepareV2Contract(f.pk, f.pk, f.n.tip().Height+10)
+
+			amount = amount.Add(payout)
+			txn.FileContracts = append(txn.FileContracts, fc)
+		}
+	}
+	{
+		i := 0
+		count := f.rng.Intn(3)
+		for id, fce := range f.v2fces {
+			if i > count {
+				break
+			}
+
+			fc := fce.V2FileContract
+			height := f.n.tip().Height
+			if height >= fc.ProofHeight {
+				continue
+			}
+			fc.RevisionNumber++
+
+			parent := fce
+			if v, ok := originalParents[id]; ok {
+				parent = v
+			} else {
+				originalParents[id] = parent
+			}
+
+			txn.FileContractRevisions = append(txn.FileContractRevisions, types.V2FileContractRevision{
+				Parent:   parent,
+				Revision: fc,
+			})
+
+			f.v2fces[id] = types.V2FileContractElement{
+				ID:             id,
+				StateElement:   fce.StateElement,
+				V2FileContract: fc,
+			}
+
+			i++
+		}
+	}
+	{
+		i := 0
+		count := f.rng.Intn(3)
+		for id, fce := range f.v2fces {
+			if i > count {
+				break
+			}
+
+			fc := fce.V2FileContract
+			height := f.n.tip().Height
+			if height < fc.ProofHeight {
+				continue
+			}
+			fc.RevisionNumber++
+
+			parent := fce
+			if v, ok := originalParents[id]; ok {
+				parent = v
+			} else {
+				originalParents[id] = parent
+			}
+
+			txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
+				Parent: parent,
+				Resolution: &types.V2StorageProof{
+					ProofIndex: f.cies[fc.ProofHeight],
+				},
+			})
+			delete(f.v2fces, id)
+
+			i++
+		}
+	}
+	// {
+	// 	i := 0
+	// 	count := f.rng.Intn(3)
+	// 	for id, fce := range f.v2fces {
+	// 		if i > count {
+	// 			break
+	// 		}
+
+	// 		fc := fce.V2FileContract
+	// 		height := f.n.tip().Height
+	// 		if height < fc.ProofHeight {
+	// 			continue
+	// 		}
+	// 		fc.RevisionNumber++
+
+	// 		parent := fce
+	// 		if v, ok := originalParents[id]; ok {
+	// 			parent = v
+	// 		} else {
+	// 			originalParents[id] = parent
+	// 		}
+
+	// 		newContract := fc
+	// 		newContract.ProofHeight = height + 10
+	// 		newContract.ExpirationHeight = newContract.ProofHeight + 1
+	// 		renewal := &types.V2FileContractRenewal{
+	// 			FinalRenterOutput: fc.RenterOutput,
+	// 			FinalHostOutput:   fc.HostOutput,
+	// 			RenterRollover:    types.ZeroCurrency,
+	// 			HostRollover:      types.ZeroCurrency,
+	// 			NewContract:       newContract,
+	// 		}
+	// 		txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
+	// 			Parent:     parent,
+	// 			Resolution: renewal,
+	// 		})
+	// 		amount = amount.Add(payoutV2(fc))
+	// 		delete(f.v2fces, id)
+
+	// 		i++
+	// 	}
+	// }
+	// {
+	// 	i := 0
+	// 	count := f.rng.Intn(3)
+	// 	for id, fce := range f.v2fces {
+	// 		if i > count {
+	// 			break
+	// 		}
+
+	// 		fc := fce.V2FileContract
+	// 		height := f.n.tip().Height
+	// 		if height < fc.ExpirationHeight {
+	// 			continue
+	// 		}
+	// 		fc.RevisionNumber++
+
+	// 		parent := fce.Copy()
+	// 		if v, ok := originalParents[id]; ok {
+	// 			parent = v
+	// 		} else {
+	// 			originalParents[id] = parent
+	// 		}
+
+	// 		txn.FileContractResolutions = append(txn.FileContractResolutions, types.V2FileContractResolution{
+	// 			Parent:     parent,
+	// 			Resolution: &types.V2FileContractExpiration{},
+	// 		})
+	// 		delete(f.v2fces, id)
+
+	// 		i++
+	// 	}
+	// }
+	{
+		// for i, count := 0, f.rng.Intn(3); i < count; i++ {
+		// 	sco := types.SiacoinOutput{
+		// 		Address: f.addr,
+		// 		Value:   types.NewCurrency64(1),
+		// 	}
+
+		// 	amount = amount.Add(sco.Value)
+		// 	txn.SiacoinOutputs = append(txn.SiacoinOutputs, sco)
+		// }
+		if amount.Cmp(types.ZeroCurrency) == 1 {
+			var sum types.Currency
+			for id, sce := range f.sces {
+				sum = sum.Add(sce.SiacoinOutput.Value)
+				txn.SiacoinInputs = append(txn.SiacoinInputs, types.V2SiacoinInput{
+					Parent:          sce,
+					SatisfiedPolicy: types.SatisfiedPolicy{Policy: f.policy},
+				})
+				delete(f.sces, id)
+
+				if sum.Cmp(amount) >= 0 {
+					break
+				}
+			}
+			if sum.Cmp(amount) == 1 {
+				txn.SiacoinOutputs = append(txn.SiacoinOutputs, types.SiacoinOutput{
+					Address: f.addr,
+					Value:   sum.Sub(amount),
+				})
+			}
+		}
+	}
+	// {
+	// 	var amount uint64
+	// 	for i, count := 0, f.rng.Intn(3); i < count; i++ {
+	// 		sfo := types.SiafundOutput{
+	// 			Address: f.addr,
+	// 			Value:   1,
+	// 		}
+
+	// 		amount += sfo.Value
+	// 		txn.SiafundOutputs = append(txn.SiafundOutputs, sfo)
+	// 	}
+	// 	if amount > 0 {
+	// 		var sum uint64
+	// 		for id, sfe := range f.sfes {
+	// 			sum += sfe.SiafundOutput.Value
+	// 			txn.SiafundInputs = append(txn.SiafundInputs, types.V2SiafundInput{
+	// 				Parent:          sfe,
+	// 				SatisfiedPolicy: types.SatisfiedPolicy{Policy: f.policy},
+	// 			})
+	// 			delete(f.sfes, id)
+
+	// 			if sum >= amount {
+	// 				break
+	// 			}
+	// 		}
+	// 		if sum > amount {
+	// 			txn.SiafundOutputs = append(txn.SiafundOutputs, types.SiafundOutput{
+	// 				Address: f.addr,
+	// 				Value:   sum - amount,
+	// 			})
+	// 		}
+	// 	}
+	// }
+	// so we don't get "transactions cannot be empty"
+	txn.ArbitraryData = []byte("1234")
+	signV2TransactionWithContracts(f.n.tipState(), f.pk, &txn)
+
+	for i := range txn.SiacoinOutputs {
+		sce := txn.EphemeralSiacoinOutput(i)
+		f.sces[sce.ID] = sce
+	}
+	for i := range txn.SiafundOutputs {
+		sfe := txn.EphemeralSiafundOutput(i)
+		f.sfes[sfe.ID] = sfe
+	}
+	return
+}
